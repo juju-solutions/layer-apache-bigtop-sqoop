5e67fc47c10124b7a5f817bd4f108c2012ec4f1f
Andrew McLeod <admcleod@gmail.com>
2016-06-07 12:46:48 +0200

    installing...

--- reactive/oozie.py
+++ reactive/oozie.py
@@ -4,36 +4,11 @@ from charms.reactive import is_state, set_state, remove_state, when, when_not
 from charms.layer.hadoop_client import get_dist_config
 
 
-@when('bigtop.available', 'mysql.available')
+@when('bigtop.available', 'db.available')
 @when_not('oozie.installed')
-def install_oozie():
+def install_oozie(db):
     hookenv.status_set('maintenance', 'Installing Oozie')
     dist = get_dist_config()
     oozie = Oozie(dist)
     oozie.install_oozie()
     set_state('oozie.installed')
-
-#@when('bigtop.available', 'oozie.installed')
-#@when_not('oozie.available')
-#def configure_oozie():
-#    hookenv.status_set('maintenance', 'Installing Pig')
-#    dist = get_dist_config
-#    oozie = Pig(dist)
-#    hadoop_ready = is_state('hadoop.ready')
-#    if hadoop_ready:
-#        hookenv.status_set('maintenance', 'configuring oozie (mapreduce)')
-#        hookenv.log('YARN is ready, configuring Apache Pig in MapReduce mode')
-#        oozie.configure_yarn()
-#        remove_state('oozie.configured.local')
-#        set_state('oozie.configured.yarn')
-#        hookenv.status_set('active', 'ready (mapreduce)')
-#        hookenv.log('Apache Pig is ready in MapReduce mode')
-#    else:
-#        hookenv.status_set('maintenance', 'configuring oozie (local)')
-#        hookenv.log('YARN is not ready, configuring Pig in local mode')
-#        oozie.configure_local()
-#        remove_state('oozie.configured.yarn')
-#        set_state('oozie.configured.local')
-#        hookenv.status_set('active', 'ready (local)')
-#        hookenv.log('Apache Pig is ready in local mode')
-#
